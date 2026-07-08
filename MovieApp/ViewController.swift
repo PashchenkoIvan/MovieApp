12651412ddddd99dc18e839c1d6787cd51373213
IvanPashchenko <100514008+PashchenkoIvan@users.noreply.github.com>
2026-07-08 11:56:41 +0200

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Ivan P. on 08/07/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MovieAppTests/MovieAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  MovieAppTests.swift
+//  MovieAppTests
+//
+//  Created by Ivan P. on 08/07/2026.
+//
+
+import Testing
+@testable import MovieApp
+
+struct MovieAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
